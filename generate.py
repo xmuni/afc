4f96e746cf6de774742147a56ed368138c549842
Engivirus <vitto.m93@gmail.com>
2019-02-06 11:58:12 +0100

    .

--- generate.py
+++ generate.py
@@ -25,6 +25,8 @@ replacements_it_home = {
     'navbutton_gallery': "Gallery",
     'navbutton_catalog': "Catalogo",
     'navbutton_about': "Contatti",
+    'intro_heading': 'AFC descrizione',
+    'intro_paragraph': 'L’Antica Fornace Carraro rappresenta un autentico baluardo dell’artigianato piovese, continuando a produrre ininterrottamente dall’Ottocento cotto di altissima qualità. La realizzazione avviene tuttora secondo le antiche tecniche produttive, utilizzando solamente argilla locale e rispettando i lenti ritmi produttivi. Le argille utilizzate vengono raccolte nei territori circostanti e i mattoni prodotti sono sottoposti ad essiccazione per tutta l’estate nelle aie all’aperto. In autunno invece, i mattoni vengono cotti nell’antico forno Hoffmann ottocentesco, autentico fulcro del complesso. Il prodotto finale si distingue nel mondo per la sua indiscussa qualità, mantenendo così vivi i saperi secolari della produzione di laterizi.',
 }
 html = render_page('home.html', replacements_it_home)
 save_html('it/index.html', html)
@@ -41,6 +43,8 @@ replacements_en_home = {
     'navbutton_gallery': "Gallery",
     'navbutton_catalog': "Catalog",
     'navbutton_about': "About",
+    'intro_heading': 'AFC description',
+    'intro_paragraph': 'Antica Fornace Carraro is an authentic bulwark of the craftsmanship of Rainforest, continuing to produce uninterruptedly since the nineteenth century terracotta of the highest quality. The realization still takes place according to ancient production techniques, using only local clay and respecting the slow pace of production. The clays used are collected in the surrounding areas and the bricks produced are dried throughout the summer in the open-air farmyards. In autumn, however, the bricks are baked in the old nineteenth-century Hoffmann oven, the real heart of the complex. The final product stands out in the world for its undisputed quality, thus keeping alive the centuries-old knowledge of brick production.',
 }
 html = render_page('home.html', replacements_en_home)
 save_html('en/index.html', html)
