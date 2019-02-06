53707ab157c39ab522e16ee1950c09a6a9628a4b
Engivirus <vitto.m93@gmail.com>
2019-02-06 11:41:22 +0100

    .

--- generate.py
+++ generate.py
@@ -5,7 +5,19 @@ def save_html(path, text):
     file.write(text)
     file.close()
 
-replacements_home = {
+# Takes 'home.html' or 'gallery.html' or any other html file containing the main block
+# And the dictionary of replacements
+def render_page(main_html, dictionary):
+    env = Environment(loader=FileSystemLoader('./sources/'))
+    template = env.get_template(main_html)
+    html_text = template.render(dictionary)
+    return html_text
+    # save_html(dictionary['name']+'.html', html_text)
+
+
+replacements_it_home = {
+    'language': 'it',
+    'name': 'index',
     'select_home': True,
     'title': 'Antica Fornace Carraro',
     'bodytext': 'Body text goes here',
@@ -14,11 +26,24 @@ replacements_home = {
     'navbutton_catalog': "Catalogo",
     'navbutton_about': "Contatti",
 }
+html = render_page('home.html', replacements_it_home)
+save_html('it/index.html', html)
+save_html('./index.html', html)
 
-print(replacements_home,'\n')
 
-env = Environment(loader=FileSystemLoader('./sources/'))
-template = env.get_template('home.html')
-result = template.render(replacements_home, language="it")
-# print(result)
-save_html('index.html', result)
+replacements_en_home = {
+    'language': 'en',
+    'name': 'index',
+    'select_home': True,
+    'title': 'Antica Fornace Carraro',
+    'bodytext': 'Body text goes here',
+    'navbutton_home': "Home",
+    'navbutton_gallery': "Gallery",
+    'navbutton_catalog': "Catalog",
+    'navbutton_about': "About",
+}
+html = render_page('home.html', replacements_en_home)
+save_html('en/index.html', html)
+
+
+print("Done")
---
+++ script.js
@@ -0,0 +1,3 @@
+console.log("Script working");
+
+// document.querySelector("#language-select").onChange = function() {console.log(this.value)};
